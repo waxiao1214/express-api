98bfa95c953acaecffe367449da45f077ced124c
xiao <wxiao1214@gmail.com>
2022-08-01 17:30:49 -0400

    initialize the project

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import fors from 'cors';
+// import { RegisterRoutes } from './routes';
+
+
+export const app = express();
